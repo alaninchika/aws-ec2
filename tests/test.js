f4a8c7c298db56a4f0991c84e41c2b650f5c277b
alaninchika <alaninchika@gmail.com>
2018-05-17 23:32:38 +1000

    change test suite

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,25 @@
 'use strict';
 
-var test = require('unit.js');
+var test = require('tape').test;
+var sinon = require('sinon');
+
 var index = require('../index.js');
 
-describe('Tests index', function() {
-    it('verifies successful response', function(done) {
-        index.handler({}, (err, response) => {
-            console.log(err)
-            console.log(response)
-        });
-    });
+test('test that succeed passes back json to context', function (assert) {
+    assert.plan(1);
+
+    var event = {
+        "operation" : "succeed",
+        "payload" : {
+            "value1" : "some value 1",
+            "value2" : "some value 2"
+        }
+    };
+
+    var context = { succeed: function (arg) {} };
+    var spy = sinon.spy(context, "succeed");
+
+    index.handler(event, context);
+
+    assert.true(spy.withArgs(event.payload).calledOnce, "Succeed on context called with payload");
 });
