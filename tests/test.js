7f6a1ddedf351bc4f3504bd8543220392df73ee1
alaninchika <alaninchika@gmail.com>
2018-05-17 23:20:19 +1000

    clean up

--- tests/test.js
+++ tests/test.js
@@ -5,15 +5,9 @@ var index = require('../index.js');
 
 describe('Tests index', function() {
     it('verifies successful response', function(done) {
-        index.get({ /* event */ }, { /* context */ }, (err, result) => {
-            try {
-                test.number(result.statusCode).is(200);
-                test.string(result.body).contains('Congratulations');
-                test.value(result).hasHeader('content-type', 'text/html');
-                done();
-            } catch(error) {
-                done(error);
-            }
+        index({}, (err, response) => {
+            console.log(err)
+            console.log(response)
         });
     });
 });
